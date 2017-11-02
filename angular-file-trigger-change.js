2e0fd02b00821040516c413bdbd4b13cd98f10a2
Luca Iaconelli <lucabelli@msn.com>
2017-11-02 15:08:31 +0100

    Aggiunto file principale.

---
+++ angular-file-trigger-change.js
@@ -0,0 +1,54 @@
+/*
+	Just add the directive to your file. Then you have the model in-sync.
+	You can use the 'ng-change' to. Ex.
+	<input
+		type="file"
+		ng-model="vm.files"
+		ng-change="vm.fileChanged()"
+		ng-file-trigger-change
+		accept="image/*"
+		capture
+		multiple
+	/>
+*/
+
+// Module
+angular
+	.module( 'ngFileTriggerChange', [] )
+	.directive( 'ngFileTriggerChange', ngFileTriggerChange )
+;
+
+// NgFileTrigger
+ngFileTriggerChange.$inject = [];
+
+function ngFileTriggerChange() {
+
+	var directive = {};
+
+	directive.restrict = 'A';
+	directive.require = '?ngModel';
+	directive.link = link;
+
+	// Linking
+	function link( scope, element, attrs, NgModelController ) {
+
+		if( ! NgModelController )
+			return;
+
+		function onChangeFunc( event ) {
+
+			var files = null;
+
+			if( event.target && event.target.files )
+				files = event.target.files;
+
+			NgModelController.$setViewValue( files );
+
+		};
+		element.change( onChangeFunc );
+
+	};
+
+	return directive;
+
+};
